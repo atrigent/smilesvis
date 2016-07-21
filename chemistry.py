9ab5fe9bfb4dd0da5e25db7aa6ebc30a6cb23bc3
Ari Entlich <atrigent@gmail.com>
2016-07-21 19:19:09 -0400

    Break up a complex line so that it is easier to understand.

--- chemistry.py
+++ chemistry.py
@@ -357,7 +357,10 @@ class Atom:
         self.geometry = get_geometry(self.steric_num, self.lone_pairs)
 
     def _add_hydrogen(self):
-        self.bonds.append(Bond(1, Atom(get_element(1), substituents=[Bond(1, self)])))
+        to_self = Bond(1, self)
+        hydrogen = Atom(get_element(1), substituents=[to_self])
+
+        self.bonds.append(Bond(1, hydrogen))
         self.bond_counts[1] += 1
 
     def __repr__(self):
